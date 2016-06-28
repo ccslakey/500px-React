e9f6eb8adf928bfb1b82e10f060872aafa8ea881
ccslakey <cromwellslakey@gmail.com>
2016-06-27 17:06:56 -0700

    added user photos at user/:id

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import request from 'superagent';
 import { CONSUMER_KEY } from '../../secrets';
 import { Row, Col, Image } from 'react-bootstrap';
+import PhotosGrid from './PhotosGrid';
 
 class UserDetails extends React.Component {
 
@@ -9,12 +10,14 @@ class UserDetails extends React.Component {
 		super(props);
 
 		this.state = {
-			user: {}
+			user: {},
+			userPhotos: [],
 		}
 	}
 
 	componentWillMount() {
 		this.getUserDetails();
+		this.getUserPhotos();
 	}
 
 	getUserDetails() {
@@ -25,6 +28,21 @@ class UserDetails extends React.Component {
 					console.log('response from 500px, setting state to res.body.user');
 					console.dir(response.body);
 					this.setState({user:response.body.user})
+				} else {
+					console.log(`Error fetching 500px`, error);
+				}
+			}
+		);
+	}
+
+	getUserPhotos(page = 1) {
+		const baseURL = `https://api.500px.com/v1/photos?feature=user&rpp=52&image_size=600,1080,1600,2048&`;
+		request.get(`${baseURL}consumer_key=${CONSUMER_KEY}&page=${page}&user_id=${this.props.params.id}`)
+			.end((error, response) => {
+				if (!error && response) {
+					console.log(`response from ${baseURL}consumer_key=${CONSUMER_KEY}&page=${page}`);
+					console.dir(response);
+					this.setState({userPhotos:response.body.photos})
 				} else {
 					console.log(`Error fetching 500px`, error);
 				}
@@ -49,13 +67,19 @@ class UserDetails extends React.Component {
 		let location = this.createLocaleString(user);
 		return (
 			<div>
-				{user.fullname}
+				<h1>{user.fullname}</h1>
 				<br/>
 				From {location}
 				<br/>
 				<Image src={user.userpic_url} thumbnail />
 				<br/>
 				Has {user.friends_count} friends
+				<h4>About</h4>
+				{user.about}
+				<h4>Photos</h4>
+
+				<PhotosGrid photos={this.state.userPhotos} />
+
 			</div>
 		);
 	}
